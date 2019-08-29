b54fac63732e1c9146d7db807b8d379b245e8835
JayTailor45 <tailorj64@gmail.com>
2019-08-29 12:31:45 +0530

    Added categories component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CategoryComponent } from './category/category.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'categories', component: CategoryComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,9 +13,10 @@ import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './common/footer/footer.component';
 import { JumbotronComponent } from './common/jumbotron/jumbotron.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, FooterComponent, JumbotronComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, FooterComponent, JumbotronComponent, CategoryComponent],
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [RouterModule],
   bootstrap: [AppComponent]
